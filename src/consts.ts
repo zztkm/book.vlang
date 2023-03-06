b245dfd4033328ed3832d3f82386fa6c5293c8a4
zztkm <takumi.basket1682@gmail.com>
2023-03-07 01:03:55 +0900

    fix: GITHUB_EDIT_URL

--- src/consts.ts
+++ src/consts.ts
@@ -20,7 +20,7 @@ export const KNOWN_LANGUAGES = {
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export const GITHUB_EDIT_URL = `https://github.com/zztkm/book.vlang`;
+export const GITHUB_EDIT_URL = `https://github.com/zztkm/book.vlang/tree/main`;
 
 export const COMMUNITY_INVITE_URL = `https://discord.gg/vlang`;
 
