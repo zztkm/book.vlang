a0129720e7957628e469ac85733c38ee9d294ab9
zztkm <takumi.basket1682@gmail.com>
2023-03-07 00:48:27 +0900

    init

--- src/consts.ts
+++ src/consts.ts
@@ -1,27 +1,28 @@
 export const SITE = {
-	title: 'Documentation',
-	description: 'Your website description.',
-	defaultLanguage: 'en-us',
+	title: 'The V Programming Language',
+	description: 'An unofficial introduction to the V language.',
+	defaultLanguage: 'ja-jp',
 } as const;
 
 export const OPEN_GRAPH = {
 	image: {
-		src: 'https://github.com/withastro/astro/blob/main/assets/social/banner-minimal.png?raw=true',
+		src: 'https://github.com/zztkm/gopher-emon/blob/main/001-gopher-emon.png?raw=true',
 		alt:
-			'astro logo on a starry expanse of space,' +
-			' with a purple saturn-like planet floating in the right foreground',
+			'the gopher-emon,' +
+			' zztkm icon',
 	},
-	twitter: 'astrodotbuild',
+	twitter: 'zztkm',
 };
 
 export const KNOWN_LANGUAGES = {
+	日本語: 'ja',
 	English: 'en',
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
+export const GITHUB_EDIT_URL = `https://github.com/zztkm/book.vlang`;
 
-export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
+export const COMMUNITY_INVITE_URL = `https://discord.gg/vlang`;
 
 // See "Algolia" section of the README for more information.
 export const ALGOLIA = {
@@ -35,6 +36,14 @@ export type Sidebar = Record<
 	Record<string, { text: string; link: string }[]>
 >;
 export const SIDEBAR: Sidebar = {
+	ja: {
+		'Section Header': [
+			{ text: 'Introduction', link: 'ja/introduction' },
+			{ text: 'Page 2', link: 'ja/page-2' },
+			{ text: 'Page 3', link: 'ja/page-3' },
+		],
+		'Another Section': [{ text: 'Page 4', link: 'ja/page-4' }],
+	},
 	en: {
 		'Section Header': [
 			{ text: 'Introduction', link: 'en/introduction' },
